daac7c545fed7c2ae672fb50f4c00f3581b117ba
cipriansuiu <suiuciprianconstantin@gmail.com>
2017-03-10 23:25:11 +0000

    new ignore

---
+++ src/Crime.java
@@ -0,0 +1,61 @@
+/**
+ * Created by Ciprian on 03/10/17.
+ */
+
+/**
+ * Class that structures the information of a crime that has been comitted
+ */
+
+public class Crime {
+
+
+
+	private Location locationName;
+	private OutcomeStatus outcomeStatus;
+	private String category;
+	private String persistentId;
+	private String id;
+	private String locationSubtype;
+	private String date;
+
+    public Crime(Location locationName, OutcomeStatus outcomeStatus, String category, String persistentId, String id, String locationSubtype, String date )
+    {
+        this.locationName = locationName;
+        this.outcomeStatus = outcomeStatus;
+        this.date = date;
+        this.category = category;
+        this.id = id;
+        this.locationSubtype = locationSubtype;
+    }
+
+    /**
+     *
+     * @return what the quantum of danger for that crime is
+     */
+    public int danger()
+    {
+        switch (this.category)
+        {
+            case ("AntiSocialbehaviour"):
+                return 1;
+            case ("burglary"):
+                return 5;
+            case ("robbery"):
+                return 20;
+            case "vehicleCrime":
+                return 5;
+            case "violentCrime":
+                return 30;
+            case "otherCrime":
+                return 10;
+             default:
+                return 0;
+        }
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
---
+++ src/OutcomeStatus.java
@@ -0,0 +1,13 @@
+import java.sql.Time;
+
+public class OutcomeStatus {
+
+	private String category;
+	private String date;
+	
+	public OutcomeStatus(String category, String date) {
+		this.category = category;
+		this.date = date;
+	}
+
+}
