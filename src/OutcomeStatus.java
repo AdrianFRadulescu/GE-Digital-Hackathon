aca2d2db9b2078c84920cdccfd408893afbd3f5a
rgp519 <rgp519@cs.bham.ac.uk>
2017-03-10 21:08:36 +0000

    OutcomeStatus

---
+++ src/OutcomeStatus.java
@@ -0,0 +1,13 @@
+import java.sql.Time;
+
+public class OutcomeStatus {
+
+	private String category;
+	private Time date;
+	
+	public OutcomeStatus(String category, Time date) {
+		this.category = category;
+		this.date = date;
+	}
+
+}
