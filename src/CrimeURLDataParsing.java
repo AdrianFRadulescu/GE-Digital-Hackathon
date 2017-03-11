6e4ef27a1bce4b5dcffb7ebf1abbda88b8257ac6
Adrian Radulescu <axr601@student.bham.ac.uk>
2017-03-11 01:08:13 +0000

    parsing class prepared for reading implementation of file reading

--- src/CrimeURLDataParsing.java
+++ src/CrimeURLDataParsing.java
@@ -1,5 +1,7 @@
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 /**
  * Created by adrian_radulescu1997 on 10/03/2017.
  */
@@ -22,98 +24,14 @@ public class CrimeURLDataParsing {
 
     }
 
-
     /**
-     * Parses a given string into a Crime object
-     * @param _args = the given string
-     * @return  a new Crime object
-     */
-
-    public static Crime praseCrime(String _args){
-
-
-
-
-        System.gc();
-
-        return null /*new Crime()*/;
-    }
-
-    /**
-     * Parses a given string into a Location object
-     * @param _args = the given string
-     * @return a new Location object
-     */
-
-    public static Location parseLocation(String _args){
-
-
-        // parse the street part
-        String auxStreet = _args.substring(_args.indexOf(fLocationParameters[0]));
-        auxStreet = auxStreet.substring(auxStreet.indexOf('{')-1);
-        System.out.println(auxStreet);
-        Street streetParam = parseStreet(auxStreet);
-
-        String auxLatitude = _args.substring(_args.indexOf(fLocationParameters[2]));
-        System.out.println(auxLatitude);
-
-        return null/*new Location()*/;
-    }
-
-
-    public static Street parseStreet(String _args){
-
-        String auxId = "";
-        for (int i = _args.indexOf(fStreetParameters[0]) + fStreetParameters[0].length() + 3; _args.charAt(i) != '}' && _args.charAt(i) != '"'; i ++){
-            auxId = auxId + _args.charAt(i);
-        }
-
-        String auxName = "";
-        for (int i = _args.indexOf(fStreetParameters[1]) + fStreetParameters[1].length() + 4; _args.charAt(i) != '\'' && _args.charAt(i) != '"'; i ++){
-            auxName = auxName + _args.charAt(i);
-        }
-
-        return new Street(auxId,auxName);
-    }
-
-    /**
-     * Takes the information for a given parameter from the string
-     * @param _crimeParameter
-     * @param _args
+     * Reads file and returns a list of the lines
+     * @param _pathToFile = the path to the file to be read
      * @return
      */
 
-    public static String getStringCorrespondingToCrimeParameter(String _crimeParameter, String _args){
+    public static ArrayList<String> readFile(String _pathToFile){
 
-        int increment = (_crimeParameter.equals("outcome_status")?5:4);
-
-        String aux = _args.substring(_args.indexOf(_crimeParameter) + _crimeParameter.length() + increment);
-        String auxRezult = "";
-        for (int i = 0; aux.charAt(i) != '\'' && aux.charAt(i) != '"';i ++){
-            auxRezult = auxRezult + aux.charAt(i);
-        }
-        System.out.println(auxRezult);
-
-        return auxRezult;
-    }
-
-    /**
-     * Returns a substring of a given string until the given character occurs
-     * @param _ch
-     * @param _args
-     * @return
-     */
-
-    public static String getStringUntilGivenCharOccurs(char _ch, String _args){
-
-        String result = "";
-        for (int i = 0; _args.charAt(i) != _ch; i ++){
-            result += _args.charAt(i);
-        }
-
-        result += _ch;
-        System.out.println(result);
-        return result;
     }
 
 }
