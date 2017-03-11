836cb58e6f93635b6dcf19cb6abf8ef734b116b2
cipriansuiu <suiuciprianconstantin@gmail.com>
2017-03-11 06:13:34 +0000

    methods implemented for statistics

--- src/Area.java
+++ src/Area.java
@@ -5,6 +5,7 @@ public class Area {
     public String nameLocation;
     Crime[] listOfCrimes;
     int dangerQuantum;
+    int numberOfCrimes;
 
     Triangle triangle;
     public Area(String nameLocation,Crime[] listOfCrimes,Triangle triangle)
@@ -14,9 +15,18 @@ public class Area {
         this.listOfCrimes=listOfCrimes;
         this.nameLocation=nameLocation;
         this.dangerQuantum=dangerZone();
+        this.numberOfCrimes=listOfCrimes.length;
+
         this.triangle=triangle;
     }
-
+    public float averageCrimesDistance()
+    {
+        return (float) (triangle.triangleArea(triangle.x.x,triangle.x.y,triangle.y.x,triangle.y.y,triangle.z.x,triangle.z.y)/numberOfCrimes);
+    }
+    public float crimesPerWeek()
+    {
+        return (float)numberOfCrimes/4;
+    }
     public int dangerZone()
     {
         int danger=0;
